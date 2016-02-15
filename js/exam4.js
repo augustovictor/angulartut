677daff5090eff7750f4dbf035340035e6c66909
Victor <victoraweb@gmail.com>
2016-02-14 21:02:31 -0300

    Fourth example

---
+++ js/exam4.js
@@ -0,0 +1,33 @@
+var app4 = angular.module('app4', []);
+
+app4.controller('app4Controller', function($scope) {
+
+	$scope.dayTimeButton = true;
+
+	$scope.capitals = [
+		{"City": "Montgomery",
+	    "State": "Alabama"},
+	    {"City": "Juneau",
+	    "State": "Alaska"},
+	    {"City": "Phoenix",
+	    "State": "Arizona"},
+	    {"City": "Little Rock",
+	    "State": "Arkansas"}
+	]
+
+	// $scope.blur = 0;
+	// $scope.click = 0;
+	// $scope.dblclick = 0;
+	// $scope.copy = 0;
+	// $scope.paste = 0;
+	// $scope.cut = 0;
+	// $scope.focus = 0;
+	// $scope.model = 0;
+	// $scope.change = 0;
+	// $scope.mouseenter = 0;
+	// $scope.mouseleave = 0;
+
+	// $scope.keydown = function(e) {
+	// 	$scope.kdKey = String.fromCharCode(e.keyCode);
+	// }
+});
